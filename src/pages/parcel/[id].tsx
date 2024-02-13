c90f5e83a44befcf04f2728cee565fe77c165f30
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2024-02-13 21:03:17 +0100

    Refactor dynamic ID parsing in parcel details page

--- src/pages/parcel/[id].tsx
+++ src/pages/parcel/[id].tsx
@@ -1,16 +1,17 @@
 import { useRouter } from "next/router";
 import Parcels, { parcelsData } from "@/components/Parcels"; 
-import Image from "next/image";
+
+
 
 const Details = () => {
   const router = useRouter();
-  const { id }  = router.query; // Get the dynamic part of the URL
+  const { id } = router.query; // Get the dynamic part of the URL
 
   // Find the parcel data matching the ID
-  const parcel = parcelsData.find((parcel) => parcel.id === parseInt(id));
+  const parcel = parcelsData.find((parcel) => parcel.id === parseInt(id as string));
 
   if (!parcel) {
-    return <div >Parcel not found</div>;
+    return <div>Parcel not found</div>;
   }
  
 
