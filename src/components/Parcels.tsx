52e17c6b361b73c9c9a54edb7fef959739ded510
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2024-02-14 00:02:14 +0100

    wip

--- src/components/Parcels.tsx
+++ src/components/Parcels.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
+
 interface ParcelsData {
   id: number;
   status: string;
@@ -73,13 +74,6 @@ export const parcelsData: ParcelsData[] = [
   },
 ];
 
-/*const Parcels:P React.FC<arcelsData> = ({
-  sender,
-  eta,
-  status,
-  location_name,
-}) => {
-  return ( */
 
 
 const Parcels = ({
@@ -93,9 +87,10 @@ const Parcels = ({
       {/* Simulated border as an inner div */}
       <div className="absolute rounded-lg  left-[2px] top-2 bottom-2 w-[6px] ml-1 bg-slate-400"></div>
 
-      <div className="flex text-slate-200  flex-col w-full pl-6 gap-2">
-        {/* Adjusted padding here for content */}
-        <h2 className="text-left text-2xl w-full">{sender}</h2>
+      <div
+        className={`flex text-slate-200 flex-col w-full pl-6 gap-2`}
+      >
+        <h2 className="text-left text-2xl  w-full">{sender}</h2>
         <p className="text-left w-full">{eta.slice(0, 10)}</p>
         <p className="text-left pl-2 pr-2 py-1 rounded-md  mr-auto  bg-slate-700">
           {status}
